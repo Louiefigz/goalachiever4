8fef5dab337d99bba592098fe6bb1e6fe4f5b230
Aaron Figueroa <aaronfigueroa@Aarons-iMac.local>
2016-07-24 20:56:06 -0400

    Devise complications.  Added route in order to make signout function operate

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+
+      def destroy
+
+      end 
+
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  devise_scope :user do
+   get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'application#index'
   namespace :api, defaults:{format: :json} do
       namespace :v1 do
         resources :goals
+        resources :users
       end
     end
 
