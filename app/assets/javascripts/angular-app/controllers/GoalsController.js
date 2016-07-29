ac7c0e5aa9e9c506d4a5332376f39877cee82471
Aaron Figueroa <aaronfigueroa@Aarons-iMac.local>
2016-07-28 21:41:54 -0400

    Goals are now able to be returned correctly with the GoalsController and goal.js service

--- app/assets/javascripts/angular-app/controllers/GoalsController.js
+++ app/assets/javascripts/angular-app/controllers/GoalsController.js
@@ -1,8 +1,9 @@
 angular
   .module('app')
   .controller('GoalsController', GoalsController);
-function GoalsController(GoalService) {
-  var ctrl = this;
-  debugger;
-  ctrl.goals = GoalService.Goals();
+
+function GoalsController(GoalService, $resource) {
+  GoalService.getGoals().then(function(resp){
+    debugger;
+  })
 }
--- app/assets/javascripts/angular-app/services/goal.js
+++ app/assets/javascripts/angular-app/services/goal.js
@@ -1,21 +1,58 @@
-
-function GoalService($resource) {
-
-  var Goals = $resource('http://localhost:3000/api/v1/goals');
-
-  var Goal = $resource('http://localhost:3000/api/v1/goals/:id', {id: '@id'}, {
-    update: { method: 'PUT' },
-    delete: {method: 'DELETE'},
-    get: { method: 'GET'}
-  });
-
-
-  return{
-    Goals: Goals,
-    Goal:Goal
-  };
-}
-
 angular
   .module('app')
-  .factory('GoalService', GoalService);
+  .service('GoalService', ['$http', '$q', function($http, $q){
+
+    this.makeAjaxCall = function(httpMethod, route, data){
+         // Create deferred object with $q
+         var deferred = $q.defer();
+
+         // Make ajax call to user route and return data
+         httpMethod(route, data).then(
+           // Success callback - return data with user info
+           function(resp){
+             deferred.resolve(resp);
+           // Error callback - return data with errors
+         }, function(resp){
+             deferred.resolve(resp);
+           }
+         );
+
+      return deferred.promise;
+    }
+
+
+
+
+    this.getGoals = function(){
+      var httpMethod = $http.get;
+      var route = '/api/v1/goals';
+      // var data = {params: {goals: goalsParams}}
+
+      return this.makeAjaxCall(httpMethod, route);
+
+    };
+
+  }]);
+
+
+
+
+
+
+
+
+
+
+  // var Goals = $resource('http://localhost:3000/api/v1/goals');
+  //
+  // var Goal = $resource('http://localhost:3000/api/v1/goals/:id.json', {id: '@id'}, {
+  //   update: { method: 'PUT' },
+  //   delete: {method: 'DELETE'},
+  //   get: { method: 'GET'}
+  // });
+  //
+  //
+  // return{
+  //   Goals: Goals,
+  //   Goal:Goal
+  // };
